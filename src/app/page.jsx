66da31c3b7b02edede0ccec0f491105abbef1e2d
Eduardo Martins <eduardobizzi2005@gmail.com>
2024-05-21 23:49:12 -0300

    Initial Commit
    Project start

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="flex w-full h-screen justify-center items-center">
+      <h1 className="text-4xl font-bold">Repair Café</h1>
+    </main>
+  )
+}
+
+export default Home
