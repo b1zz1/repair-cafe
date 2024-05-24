af0565d7e278488b7a6c58efe31df532b432b9fe
ALenckulh <127156754+ALenckulh@users.noreply.github.com>
2024-05-24 17:02:33 -0300

    Visual da página Home concluído

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,34 @@
-import Header from '@components/layout/Header'
+
+import Header from "@components/layout/Header";
+import { PiMagnifyingGlass } from "react-icons/pi";
+import { Input } from "@/components/ui/input";
+import wave from '../midia/wave.svg';
+import Image from "next/image";
+
+let name = "Ana";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <main className="flex w-full min-h-screen justify-center items-center bg-purple.1">
-        <h1 className="text-4xl font-bold">Repair Café</h1>
+      <main className="flex w-full h-screen flex-col items-center bg-purple.1">
+        <Header />
+        <div className="flex h-full flex-col items-center justify-between pt-40 ">
+          <div div className="flex w-fit flex-col gap-8 px-12" >
+            <div>
+              <h1 className="text-5xl text-purple.5">
+                Ola, <b>{name}</b>
+              </h1>
+              <h1 className="text-5xl text-purple.5">
+                O que vamos reparar hoje?
+              </h1>
+            </div>
+            <Input placeholder="Search..." prepend={<PiMagnifyingGlass />} />
+          </div>
+          <Image src={wave} alt="Wave" />
+        </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,15 @@
-const Header = () => {
-    return (
-        <header className="flex w-full h-20">
-            <h3 className="text-2xl">Logo</h3>
-        </header>
-    )
-}
+import { Button } from "@/components/ui/button";
+import { PiUserBold } from "react-icons/pi";
 
-export default Header
+const Header = () => {
+  return (
+    <header className="flex w-full h-20 justify-between text-purple.5 bg-white.1 items-center px-8">
+      <h3 className="text-2xl">Logo</h3>
+        <Button size="avatar" className="rounded-full bg-purple.3 hover:bg-purple.5/25">
+            <PiUserBold className="h-4 w-4" />
+        </Button>
+    </header>
+  );
+};
+
+export default Header;
