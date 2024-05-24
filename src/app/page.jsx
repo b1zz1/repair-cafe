9cbaa2acb44d2e9c1b890f2c3459d6930aa3e056
Eduardo Martins <eduardobizzi2005@gmail.com>
2024-05-23 23:19:33 -0300

    Starting home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,13 @@
+import Header from '@components/layout/Header'
+
 const Home = () => {
   return (
-    <main className="flex w-full h-screen justify-center items-center">
-      <h1 className="text-4xl font-bold">Repair Café</h1>
-    </main>
+    <>
+      <Header />
+      <main className="flex w-full min-h-screen justify-center items-center bg-purple.1">
+        <h1 className="text-4xl font-bold">Repair Café</h1>
+      </main>
+    </>
   )
 }
 
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex w-full h-20">
+            <h3 className="text-2xl">Logo</h3>
+        </header>
+    )
+}
+
+export default Header
