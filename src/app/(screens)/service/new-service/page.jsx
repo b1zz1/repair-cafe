3b8c1a62108450e367a13dbb453ad6f1361bf917
ALenckulh <127156754+ALenckulh@users.noreply.github.com>
2024-06-16 22:21:24 -0300

    update new service

--- src/app/(screens)/service/new-service/page.jsx
+++ src/app/(screens)/service/new-service/page.jsx
@@ -1,6 +1,7 @@
 //http://localhost:3000/service/new-service
 "use client";
 
+import React from "react";
 import Header from "@components/layout/Header";
 import WaveCafe from "@/components/ui/waveCafe";
 import IconCafe from "@/components/ui/iconCafe";
@@ -19,59 +20,133 @@ import {
 } from "react-icons/pi";
 
 const NewService = () => {
+  const [content, setContent] = React.useState("Basico");
+
   return (
     <section className="flex w-full h-screen flex-col bg-purple.1 overflow-hidden">
       <Header />
       <main className="flex w-full h-full flex-col justify-between pt-12">
         <div className="flex h-full w-full flex-col justify-center items-center gap-10">
-          <div className="flex flex-row items-center">
-            <div className="bg-gradient-to-r from-purple.4 to-purple.6 p-2.5 rounded-full">
-              <IconCafe Icon={PiStorefront} Colors="white" />
-            </div>
-            <LineCafe />
-            <div className="bg-purple.3 p-2.5 rounded-full">
-              <IconCafe Icon={PiPhone} />
-            </div>
-            <LineCafe />
-            <div className="bg-purple.3 p-2.5 rounded-full">
-              <IconCafe Icon={PiMapPin} />
-            </div>
-          </div>
-          <div className="flex flex-col gap-8 py-8 pt-0">
-            <h1 className="text-xl text-purple.5 select-none sm:text-4xl">
-              Informações Básicas
-            </h1>
-            <div className="flex flex-col gap-5">
-              <Input
-                type="text"
-                size="g"
-                placeholder="Nome"
-                prepend={<IconCafe Icon={PiStorefront} />}
-              />
-              <Input
-                id="Picture"
-                type="file"
-                accept="image/png, image/jpeg"
-                size="g"
-                prepend={<IconCafe Icon={PiCamera} />}
-                placeholderFile="Foto"
-              />
-              <SelectCafe
-                size="g"
-                placeholder="Especialidade (Opcional)"
-                IconLeft={PiSparkle}
-              />
-              <Textarea placeholder="Escreva a descrição aqui" />
-            </div>
-            <div className="flex justify-between w-full gap-5">
-              <Button variant="outline" size="lg">
-                Cancelar
-              </Button>
-              <Button type="submit" variant="default" size="lg">
-                Confirmar
-              </Button>
-            </div>
-          </div>
+          {content === "Basico" ? (
+            <>
+              <div className="flex flex-row items-center">
+                <div
+                  className="bg-gradient-to-r from-purple.4 to-purple.6 p-2.5 rounded-full"
+                >
+                  <IconCafe Icon={PiStorefront} Colors="white" />
+                </div>
+                <LineCafe />
+                <div className="bg-purple.3 p-2.5 rounded-full">
+                  <IconCafe Icon={PiPhone} />
+                </div>
+                <LineCafe />
+                <div className="bg-purple.3 p-2.5 rounded-full">
+                  <IconCafe Icon={PiMapPin} />
+                </div>
+              </div>
+              <div className="flex flex-col gap-8 py-8 pt-0">
+                <h1 className="text-xl text-purple.5 select-none sm:text-4xl">
+                  Informações Básicas
+                </h1>
+                <div className="flex flex-col gap-5">
+                  <Input
+                    type="text"
+                    size="g"
+                    placeholder="Nome"
+                    prepend={<IconCafe Icon={PiStorefront} />}
+                  />
+                  <Input
+                    id="Picture"
+                    type="file"
+                    accept="image/png, image/jpeg"
+                    size="g"
+                    prepend={<IconCafe Icon={PiCamera} />}
+                    placeholderFile="Foto"
+                  />
+                  <SelectCafe
+                    size="g"
+                    placeholder="Especialidade (Opcional)"
+                    IconLeft={PiSparkle}
+                  />
+                  <Textarea placeholder="Escreva a descrição aqui" />
+                </div>
+                <div className="flex justify-between w-full gap-5">
+                  <Button variant="outline" size="lg">
+                    Cancelar
+                  </Button>
+                  <Button
+                    type="submit"
+                    variant="default"
+                    size="lg"
+                    onClick={() => setContent("Contato")}
+                  >
+                    Confirmar
+                  </Button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-row items-center">
+                <div
+                  className="bg-gradient-to-r from-purple.4 to-purple.6 p-2.5 rounded-full"
+                  onClick={() => setContent("Basico")}
+                >
+                  <IconCafe Icon={PiStorefront} Colors="white" />
+                </div>
+                <LineCafe />
+                <div
+                  className="bg-gradient-to-r from-purple.4 to-purple.6 p-2.5 rounded-full"
+                >
+                  <IconCafe Icon={PiPhone} Colors="white" />
+                </div>
+                <LineCafe />
+                <div className="bg-purple.3 p-2.5 rounded-full">
+                  <IconCafe Icon={PiMapPin} />
+                </div>
+              </div>
+              <div className="flex flex-col gap-8 py-8 pt-0">
+                <h1 className="text-xl text-purple.5 select-none sm:text-4xl">
+                  Informações Básicas
+                </h1>
+                <div className="flex flex-col gap-5">
+                  <Input
+                    type="text"
+                    size="g"
+                    placeholder="Nome"
+                    prepend={<IconCafe Icon={PiStorefront} />}
+                  />
+                  <Input
+                    id="Picture"
+                    type="file"
+                    accept="image/png, image/jpeg"
+                    size="g"
+                    prepend={<IconCafe Icon={PiCamera} />}
+                    placeholderFile="Foto"
+                  />
+                  <SelectCafe
+                    size="g"
+                    placeholder="Especialidade (Opcional)"
+                    IconLeft={PiSparkle}
+                  />
+                  <Textarea placeholder="Escreva a descrição aqui" />
+                </div>
+                <div className="flex justify-between w-full gap-5">
+                  <Button variant="outline" size="lg">
+                    Cancelar
+                  </Button>
+                  <Button
+                    type="submit"
+                    variant="default"
+                    size="lg"
+                    onClick={() => setContent("Contato")}
+                  >
+                    Confirmar
+                  </Button>
+                </div>
+              </div>
+            </>
+          )}
         </div>
         <WaveCafe variant={"alt"} />
       </main>
