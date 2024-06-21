fece4be5125f276136fb7a3739df4de3b014411e
Eduardo Bizzi Martins <eduardobizzi2005@gmail.com>
2024-06-21 02:58:33 -0300

    Service Creation
    Fixed and integrated service creation with back end

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -44,6 +44,7 @@ const buttonVariants = cva(
 const Button = React.forwardRef(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+    console.log(props)
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
